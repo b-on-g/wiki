d8418dc0205237af4c00db2b99cbe69ded4dd893
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-04-14 22:24:24 +0300

    Add debug logging to registry/page create

--- editor/editor.view.ts
+++ editor/editor.view.ts
@@ -13,21 +13,25 @@ namespace $.$$ {
 		/** Current page land link from URL */
 		@ $mol_mem
 		override page_land_link( next?: string ) {
+			const val = this.$.$mol_state_arg.value( 'page' ) ?? ''
+			console.log('[wiki] page_land_link', next !== undefined ? 'WRITE:' + next : 'READ:' + val)
 			if( next !== undefined ) {
 				this.$.$mol_state_arg.value( 'page', next || null )
 				return next
 			}
-			return this.$.$mol_state_arg.value( 'page' ) ?? ''
+			return val
 		}
 
 		/** Registry land link from URL */
 		@ $mol_mem
 		registry_land_link( next?: string ) {
+			const val = this.$.$mol_state_arg.value( 'registry' ) ?? ''
+			console.log('[wiki] registry_land_link', next !== undefined ? 'WRITE:' + next : 'READ:' + val)
 			if( next !== undefined ) {
 				this.$.$mol_state_arg.value( 'registry', next || null )
 				return next
 			}
-			return this.$.$mol_state_arg.value( 'registry' ) ?? ''
+			return val
 		}
 
 		/** User data from home land. Do NOT put @$mol_mem. */
@@ -349,28 +353,35 @@ namespace $.$$ {
 		/** Create new registry land, add to home, switch to it */
 		@ $mol_action
 		registry_create( event?: Event ) {
+			console.log('[wiki] registry_create called', event)
 			if( !event ) return null
 
-			const land = this.$.$giper_baza_glob.land_grab(
-				[[ null, $giper_baza_rank_post( 'just' ) ]]
-			)
+			try {
+				const land = this.$.$giper_baza_glob.land_grab(
+					[[ null, $giper_baza_rank_post( 'just' ) ]]
+				)
 
-			// Init registry with empty title
-			const data = land.Data( $bog_wysiwyg_model_registry )
-			data.Title( 'auto' )?.val( '' )
+				// Init registry with empty title
+				const data = land.Data( $bog_wysiwyg_model_registry )
+				data.Title( 'auto' )?.val( '' )
 
-			const link_str = land.link().str
+				const link_str = land.link().str
 
-			// Add to home
-			this.user_registries_add( link_str )
+				// Add to home
+				this.user_registries_add( link_str )
 
-			// Switch to it
-			this.registry_land_link( link_str )
+				// Switch to it
+				this.registry_land_link( link_str )
 
-			// Create first page
-			this.page_create( new Event( 'auto' ) )
+				// Create first page
+				this.page_create( new Event( 'auto' ) )
 
-			return event
+				console.log('[wiki] registry_create done, link:', link_str)
+				return event
+			} catch( e ) {
+				console.error('[wiki] registry_create FAILED', e)
+				throw e
+			}
 		}
 
 		/** Page rows for sidebar */
@@ -414,29 +425,36 @@ namespace $.$$ {
 		/** Create new page land and add to registry */
 		@ $mol_action
 		page_create( event?: Event ) {
+			console.log('[wiki] page_create called', event)
 			if( !event ) return null
 
-			const reg = this.registry_ensure()
+			try {
+				const reg = this.registry_ensure()
 
-			const land = this.$.$giper_baza_glob.land_grab(
-				[[ null, $giper_baza_rank_post( 'just' ) ]]
-			)
+				const land = this.$.$giper_baza_glob.land_grab(
+					[[ null, $giper_baza_rank_post( 'just' ) ]]
+				)
 
-			// Init page with empty title
-			const data = land.Data( $bog_wysiwyg_model_page )
-			data.Title( 'auto' )?.val( '' )
+				// Init page with empty title
+				const data = land.Data( $bog_wysiwyg_model_page )
+				data.Title( 'auto' )?.val( '' )
 
-			// Add to registry via Pages
-			const pages = reg.Pages( 'auto' )
-			if( pages ) {
-				const current = pages.items_vary() ?? []
-				pages.items_vary([ ...current, land.link() ])
+				// Add to registry via Pages
+				const pages = reg.Pages( 'auto' )
+				if( pages ) {
+					const current = pages.items_vary() ?? []
+					pages.items_vary([ ...current, land.link() ])
+				}
+
+				// Navigate to new page
+				this.page_land_link( land.link().str )
+
+				console.log('[wiki] page_create done')
+				return event
+			} catch( e ) {
+				console.error('[wiki] page_create FAILED', e)
+				throw e
 			}
-
-			// Navigate to new page
-			this.page_land_link( land.link().str )
-
-			return event
 		}
 
 		/** Navigate to a page (from graph click) */
